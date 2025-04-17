387c02297557dc8c16848fd85e4daaf458d2c03d
Kenneth Kipchumba <branrono@gmail.com>
2025-04-17 17:14:22 +0300

    1. Composer installed livewire 2. Implemented counter component using livewire.

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <h1>Care about people's approval and you will be their prisoner.</h1>
+    <h2>{{ $count }}</h2>
+
+    <button wire:click="increment">
+        +
+    </button>
+
+    <button wire:click="decrement">
+        -
+    </button>
+</div>
